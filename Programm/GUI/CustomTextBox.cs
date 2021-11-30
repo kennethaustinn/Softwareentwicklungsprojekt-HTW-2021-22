51a73b72aaf617009ea3ba7eeaf1ca8ce2211ff5
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2021-11-30 02:06:34 +0100

    die Datei im neue Folder verschoben

---
+++ Programm/GUI/CustomTextBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class CustomTextBox : UserControl
+    {
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        public CustomTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public Color BorderColor { get => borderColor; set => borderColor = value;}
+        public int BorderSize { get => borderSize; set => borderSize = value; }
+        public bool UnderlinedStyle { get => underlinedStyle; set => underlinedStyle = value; }
+    }
+}
---
+++ Programm/GUI/MitarbeiterListe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class MitarbeiterListe : Form
+    {
+        public MitarbeiterListe()
+        {
+            InitializeComponent();
+        }
+
+        private void MitarbeiterListe_Shown(object sender, EventArgs e)
+        {
+            MitarbeiterListeTable.Rows.Add(
+                new object[]
+                {
+                    1,
+                    "Mustermann",
+                    "Müller",
+                    "Design",
+                    "Bildverarbeitung"
+                }
+                );
+            MitarbeiterListeTable.Rows.Add(
+               new object[]
+               {
+                    2,
+                    "Morris",
+                    "Philip",
+                    "Coding",
+                    "Programmierung"
+               }
+               );
+        }
+
+
+        private void MitarbeiterListeTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0)
+            {
+                Profile form1 = new Profile();
+                form1.ShowDialog();
+            }
+        }
+    }
+}
