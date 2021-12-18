685560eafb0646e8e793cbb589a8d07dbecd86f4
Kenneth Austin <s0574052@htw-berlin.de>
2021-12-18 22:02:01 +0100

    Registieren ein neue Mitarbeiter

---
+++ Programm/GUI/insertData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GUI { 
+    class insertData
+    {
+        Connection con = new Connection();
+        Encrypt en = new Encrypt();
+
+        public string InsertData(string userInsert, string passInsert)
+        {
+            try
+            {
+                Connection.DataSource();
+                con.connOpen();
+                MySqlCommand command = new MySqlCommand();
+                command.CommandText = "INSERT INTO users (userName, Password) values (@name , @password)";
+                command.Parameters.AddWithValue("@name", userInsert);
+                command.Parameters.AddWithValue("@password", Encrypt.HashString(passInsert));
+                command.Connection = Connection.connMaster;
+                command.ExecuteNonQuery();
+                System.Windows.Forms.MessageBox.Show("Account created");
+                con.connClose();
+
+                return userInsert + passInsert;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                con.connClose();
+            }
+        }
+    }
+}
