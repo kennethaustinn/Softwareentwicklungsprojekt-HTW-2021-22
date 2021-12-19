098b44df20707e40e9509e1020e9299e8b1b04aa
Kenneth Austin <s0574052@htw-berlin.de>
2021-12-19 12:25:17 +0100

    Salting Password hinzugefügt

--- Programm/GUI/Encrypt.cs
+++ Programm/GUI/Encrypt.cs
@@ -11,16 +11,26 @@ namespace GUI
     {
         public static string HashString(string passwordString)
         {
+            string salt = CreateSalt(10);
             StringBuilder sb = new StringBuilder();
-            foreach (byte b in GetHash(passwordString))
+            foreach (byte b in GetHash(passwordString, salt))
                 sb.Append(b.ToString("X3"));
             return sb.ToString();
         }
-
-        public static byte[] GetHash (string passwordString)
+        public static string CreateSalt(int size)
+        {
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                var salt = new byte[size];
+                generator.GetBytes(salt);
+                return Convert.ToBase64String(salt);
+            }
+        }
+        public static byte[] GetHash (string passwordString, string salt)
         {
             using (HashAlgorithm algorithm = SHA256.Create())
-                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(passwordString));
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(passwordString + salt));
         }
+        
     }
 }
