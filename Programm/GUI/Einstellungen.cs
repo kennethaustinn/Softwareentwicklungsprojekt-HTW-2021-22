a5847825505d17a139e62fc0e7d142f6a9b9a058
Kenneth Austin <s0574052@htw-berlin.de>
2021-12-19 20:16:27 +0100

    Nutzer kann Benutzername ändern

--- Programm/GUI/Einstellungen.cs
+++ Programm/GUI/Einstellungen.cs
@@ -5,16 +5,36 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace GUI
 {
     public partial class Einstellungen : Form
     {
+        Connection con = new Connection();
         public Einstellungen()
         {
             InitializeComponent();
         }
 
-
+        private void buttonChangeBenutzername_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Connection.DataSource();
+                con.connOpen();
+                MySqlCommand command = new MySqlCommand();
+                command.CommandText = ("update mitarbeiter set Benutzername ='" + txtBenutzername.Text + "' where Benutzername ='" + Hauptseite.hauptseite.Username.Text + "'");
+                command.Connection = Connection.connMaster;
+                command.ExecuteNonQuery();
+                MessageBox.Show("Benutzername aktualisiert");
+                con.connClose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
     }
 }
