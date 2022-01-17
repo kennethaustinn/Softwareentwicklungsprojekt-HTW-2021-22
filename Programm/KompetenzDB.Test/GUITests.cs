dbcf57301edd2fb450df7fd2edf679a179c0c05c
Kenneth Austin <s0574052@htw-berlin.de>
2022-01-17 16:14:22 +0100

    UnitTest hinzugefügt

---
+++ Programm/KompetenzDB.Test/GUITests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using GUI;
+
+namespace KompetenzDB.Test
+{
+    public class GUITests
+    {
+
+        [Test]
+        public void Selecting_Data_Login()
+        {
+            // arrange
+
+            // act
+
+
+            // assert
+            Assert.Fail();
+
+        }
+    }
+}
